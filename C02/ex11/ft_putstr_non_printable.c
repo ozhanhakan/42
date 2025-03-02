db329aabd718deb47c22c8a347abe0a6eff087ba
Hakan Özhan <ozhanhakan@gmail.com>
2025-03-03 01:56:39 +0300

    ex11/ft_putstr_nonprint completed

---
+++ C02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hozhan <hozhan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 00:40:46 by hozhan            #+#    #+#             */
+/*   Updated: 2025/03/03 01:56:14 by hozhan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_puthex(unsigned char c)
+{
+	char	*hex;
+
+	hex = "0123456789ABCDEF";
+	ft_putchar('\\');
+	ft_putchar(hex[c / 16]);
+	ft_putchar(hex[c % 16]);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] > 31 && str[i] < 127 )
+			ft_putchar(str[i]);
+		else
+			ft_puthex(str[i]);
+		i++;
+	}
+}
+/* 
+#include <stdio.h>
+
+void	ft_putstr_non_printable(char *str);
+
+int	main(void)
+{
+	char	str1[50] = "Bu \n bir satirbasi karakteri mi ?";
+	char	str2[50] = "Bu da \x07 bir zil sesi!";
+
+	ft_putstr_non_printable(str1);
+	ft_putchar('\n');
+	ft_putstr_non_printable(str2);
+	ft_putchar('\n');
+	return (0);
+} */
