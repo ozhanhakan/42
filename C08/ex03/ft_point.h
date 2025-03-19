14180035fa85148feab4b049f9147f24b48739f0
Hakan Özhan <ozhanhakan@gmail.com>
2025-03-19 22:37:45 +0300

    ex03 ft_point.h added

---
+++ C08/ex03/ft_point.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hozhan <hozhan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 22:24:58 by hozhan            #+#    #+#             */
+/*   Updated: 2025/03/19 22:36:58 by hozhan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_POINT_H
+# define FT_POINT_H
+
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
+void	set_point(t_point *point);
+#endif
