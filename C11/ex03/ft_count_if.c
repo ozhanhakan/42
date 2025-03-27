c82e332d66edba29592e6e6faf077e90294dc5ed
Hakan Özhan <ozhanhakan@gmail.com>
2025-03-27 13:52:09 +0300

    a different solution with index i

--- C11/ex03/ft_count_if.c
+++ C11/ex03/ft_count_if.c
@@ -6,23 +6,26 @@
 /*   By: hozhan <hozhan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 23:42:46 by hozhan            #+#    #+#             */
-/*   Updated: 2025/03/27 13:46:41 by hozhan           ###   ########.fr       */
+/*   Updated: 2025/03/27 13:51:32 by hozhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_count_if(char **tab, int length, int (*f)(char*))
 {
 	int	count;
+	int	i;
 
 	count = 0;
-	while (length--)
+	i = 0;
+	while (i < length)
 	{
-		if (f(tab[length]))
+		if (f(tab[i]))
 			count++;
+		i++;
 	}
 	return (count);
 }
-
+/* 
 
 #include <stdio.h>
 
@@ -41,4 +44,4 @@ int	main(void)
 	printf("Test 2: %d\n",
 	 ft_count_if(tab2,sizeof(tab2)/sizeof(tab2[0]) - 1, &is_not_empty));
 	return (0);
-}
+} */
