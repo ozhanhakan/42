5e411f944993ede974c46ae3ff17d69d896f6a54
Hakan Özhan <ozhanhakan@gmail.com>
2025-04-28 18:13:21 +0300

    stdlib library added to h file

--- C12/ex06/ft_list.h
+++ C12/ex06/ft_list.h
@@ -6,12 +6,14 @@
 /*   By: hozhan <hozhan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 15:00:56 by hozhan            #+#    #+#             */
-/*   Updated: 2025/04/28 17:17:28 by hozhan           ###   ########.fr       */
+/*   Updated: 2025/04/28 18:02:54 by hozhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LIST_H
 # define FT_LIST_H
+
+# include <stdlib.h>
 
 typedef struct s_list
 {
--- C12/ex06/ft_list_clear.c
+++ C12/ex06/ft_list_clear.c
@@ -6,11 +6,11 @@
 /*   By: hozhan <hozhan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 15:00:02 by hozhan            #+#    #+#             */
-/*   Updated: 2025/04/28 17:18:31 by hozhan           ###   ########.fr       */
+/*   Updated: 2025/04/28 18:02:42 by hozhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "ft_list.h"
 
 void	ft_list_clear(t_list *begin_list, void (*free_fct)(void *))
 {
