0aa3af8061d3ad7a4826ed16fa4c4f66d02374be
hozhan <hozhan@student.42kocaeli.com.tr>
2025-06-11 17:19:49 +0300

    test changed

--- Libft/tests/manual_tests/ft_memcpy_manual_test.c
+++ Libft/tests/manual_tests/ft_memcpy_manual_test.c
@@ -5,7 +5,7 @@
 
 #include <stddef.h>
 
-/* void	*ft_memcpy(void *dst, const void *src, size_t n)
+/* void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned char		*d;
 	unsigned const char	*s;
@@ -17,13 +17,13 @@
 	return (dst);
 } */
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t		i;
 	char		*d;
 	const char	*s;
 
-	d = dst;
+	d = dest;
 	s = src;
 	i = 0;
 	if (!d && !s)
@@ -33,7 +33,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 		d[i] = s[i];
 		i++;
 	}
-	return (dst);
+	return (dest);
 }
 
 void print_buffers(const char *label, const char *buf) {
--- Libft/tests/manual_tests/ft_memmove_manual_test.c
+++ Libft/tests/manual_tests/ft_memmove_manual_test.c
@@ -2,6 +2,32 @@
 #include <string.h>
 #include "../../src/libft.h"
 
+#include <stddef.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (!dest && !src)
+		return ((void *)dest);
+	if (d < s)
+	{
+		while (n--)
+			*d++ = *s++;
+	}
+	else
+	{
+		d += n;
+		s += n;
+		while (n--)
+			*--d = *--s;
+	}
+	return ((void *)dest);
+}
+
 
 void	test_memmove(void)
 {
