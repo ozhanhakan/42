2e418bfc7352aa66d238fea7d48cfa292bd9d440
hozhan <hozhan@student.42kocaeli.com.tr>
2025-06-11 17:19:40 +0300

    memcpy parameter name changed why?

--- Libft/src/ft_memcpy.c
+++ Libft/src/ft_memcpy.c
@@ -6,20 +6,20 @@
 /*   By: hozhan <hozhan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 07:59:38 by hozhan            #+#    #+#             */
-/*   Updated: 2025/06/07 17:22:16 by hozhan           ###   ########.fr       */
+/*   Updated: 2025/06/11 16:04:29 by hozhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned char		*d;
 	unsigned const char	*s;
 
-	d = (unsigned char *)dst;
+	d = (unsigned char *)dest;
 	s = (unsigned const char *)src;
 	while (n--)
 		*d++ = *s++;
-	return (dst);
+	return (dest);
 }
