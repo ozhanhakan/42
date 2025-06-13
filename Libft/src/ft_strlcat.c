3e0ebf5656170ff2cb59ca255fde87e1501dde7d
hozhan <hozhan@student.42kocaeli.com.tr>
2025-06-13 18:18:58 +0300

    strlcat update -failed

--- Libft/src/ft_strlcat.c
+++ Libft/src/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hozhan <hozhan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 16:53:57 by hozhan            #+#    #+#             */
-/*   Updated: 2025/06/13 18:08:10 by hozhan           ###   ########.fr       */
+/*   Updated: 2025/06/13 18:18:46 by hozhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,17 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	dst_len;
 	size_t	src_len;
-	size_t	remaining;
+	size_t	i;
 
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
-	remaining = size - 1 - dst_len;
 	if (size <= dst_len)
 		return (src_len + size);
-	while(src && remaining > 0)
+	i = 0;
+	while(i < size - 1 - dst_len)
 	{
-		*(dst + dst_len) = *src++;
-		dst++;
-		remaining--;
+		*(dst + dst_len + i) = *src++;
+		i++;
 	}
 	*dst = '\0';
 	return (dst_len + src_len);
