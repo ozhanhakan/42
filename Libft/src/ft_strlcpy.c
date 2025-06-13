8763b544dbf70461b0b6677795d4142b9016b2b8
hozhan <hozhan@student.42kocaeli.com.tr>
2025-06-13 16:53:32 +0300

    strlcpy tested

--- Libft/src/ft_strlcpy.c
+++ Libft/src/ft_strlcpy.c
@@ -6,7 +6,33 @@
 /*   By: hozhan <hozhan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 14:59:57 by hozhan            #+#    #+#             */
-/*   Updated: 2025/06/13 14:59:58 by hozhan           ###   ########.fr       */
+/*   Updated: 2025/06/13 16:53:02 by hozhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+
+static size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s++)
+		len++;
+	return (len);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	len;
+
+	len = ft_strlen(src);
+	if (size == 0)
+		return (len);
+	while (*src && --size)
+	{
+		*dst++ = *src++;
+	}
+	*dst = '\0';
+	return (len);
+}
