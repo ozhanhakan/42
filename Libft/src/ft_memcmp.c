b7ba467bafa7a114a1c4ff1bcf7ce29c43a356f1
hozhan <hozhan@student.42kocaeli.com.tr>
2025-06-13 11:02:41 +0300

    memcmp added

--- Libft/src/ft_memcmp.c
+++ Libft/src/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: hozhan <hozhan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 20:45:19 by hozhan            #+#    #+#             */
-/*   Updated: 2025/06/12 20:46:44 by hozhan           ###   ########.fr       */
+/*   Updated: 2025/06/13 10:44:17 by hozhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	
+	const unsigned char	*ptr1;
+	const unsigned char	*ptr2;
+
+	if (n == 0)
+		return (0);
+	ptr1 = (const unsigned char *)s1;
+	ptr2 = (const unsigned char *)s2;
+	while (n)
+	{
+		if (*ptr1 != *ptr2)
+			return (*ptr1 - *ptr2);
+		ptr1++;
+		ptr2++;
+		n--;
+	}
+	return (0);
 }
