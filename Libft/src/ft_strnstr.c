5ee52067ba8591b179c3259b9c364f7f98702c91
Hakan Özhan <ozhanhakan@gmail.com>
2025-06-15 19:38:47 +0300

    ft_strnstr added

--- Libft/src/ft_strnstr.c
+++ Libft/src/ft_strnstr.c
@@ -6,13 +6,30 @@
 /*   By: ubuntuhakan <ubuntuhakan@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 16:49:52 by ubuntuhakan       #+#    #+#             */
-/*   Updated: 2025/06/15 16:50:46 by ubuntuhakan      ###   ########.fr       */
+/*   Updated: 2025/06/15 19:22:39 by ubuntuhakan      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	
+	size_t	i;
+	size_t	j;
+	size_t	little_len;
+
+	little_len = ft_strlen(little);
+	i = 0;
+	if (little[i] == '\0')
+		return ((char *)big);
+	while (big[i] && i + little_len <= len)
+	{
+		j = 0;
+		while ((i + j) < len && little[j] && little[j] == big[i + j])
+			j++;
+		if (little[j] == '\0')
+			return ((char *)&big[i]);
+		i++;
+	}
+	return (0);
 }
