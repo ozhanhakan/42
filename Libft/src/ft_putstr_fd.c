858afb60bb59d2908c73b1514832b0001f346b8a
Hakan Özhan <ozhanhakan@gmail.com>
2025-06-19 13:15:52 +0300

    putstr_fd added

---
+++ Libft/src/ft_putstr_fd.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hozhan <hozhan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/19 11:48:42 by hozhan            #+#    #+#             */
+/*   Updated: 2025/06/19 13:15:30 by hozhan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stddef.h>
+#include "libft.h"
+
+/**
+ * Outputs the string ’s’ to the specified file descriptor.
+ */
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	len;
+
+	if (!s)
+		return ;
+	len = ft_strlen(s);
+	write(fd, s, len);
+}
