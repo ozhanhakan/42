db79e1f3ac68e0fa3476edfde9fe91a2009da647
Hakan Özhan <ozhanhakan@gmail.com>
2025-06-19 13:17:12 +0300

    putchar function comment added

--- Libft/src/ft_putchar_fd.c
+++ Libft/src/ft_putchar_fd.c
@@ -6,13 +6,15 @@
 /*   By: hozhan <hozhan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 23:39:06 by hozhan            #+#    #+#             */
-/*   Updated: 2025/06/18 23:43:22 by hozhan           ###   ########.fr       */
+/*   Updated: 2025/06/19 13:16:25 by hozhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-/***Outputs the character ’c’ to the specified file descriptor.*/
+/**
+ * Outputs the character ’c’ to the specified file descriptor.
+ */
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
