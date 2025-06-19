78f136e6dcb07c9fa4d20993f008558049001a41
Hakan Özhan <ozhanhakan@gmail.com>
2025-06-19 13:31:38 +0300

    doxygen comment update

--- Libft/src/ft_putchar_fd.c
+++ Libft/src/ft_putchar_fd.c
@@ -6,16 +6,16 @@
 /*   By: hozhan <hozhan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 23:39:06 by hozhan            #+#    #+#             */
-/*   Updated: 2025/06/19 13:25:13 by hozhan           ###   ########.fr       */
+/*   Updated: 2025/06/19 13:29:20 by hozhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 /**
- * @brief Outputs a single character to the specified file descriptor.
- * @param c Character to be written
- * @param fd File descriptor for the output operation
+ * @brief Outputs the character ’c’ to the specified file descriptor.
+ * @param c CThe character to output.
+ * @param fd The file descriptor on which to write.
  */
 void	ft_putchar_fd(char c, int fd)
 {
--- Libft/src/ft_putstr_fd.c
+++ Libft/src/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: hozhan <hozhan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 11:48:42 by hozhan            #+#    #+#             */
-/*   Updated: 2025/06/19 13:15:30 by hozhan           ###   ########.fr       */
+/*   Updated: 2025/06/19 13:31:14 by hozhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 #include "libft.h"
 
 /**
- * Outputs the string ’s’ to the specified file descriptor.
+ * @brief Outputs the string ’s’ to the specified file descriptor.
+ * @param s The string to output.
+ * @param fd The file descriptor on which to write.
  */
 void	ft_putstr_fd(char *s, int fd)
 {
