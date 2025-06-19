f11f18f8db01d45ed630ca39a427b9ee6ef025b0
Hakan Özhan <ozhanhakan@gmail.com>
2025-06-19 13:27:09 +0300

    Doxygen comment style added

--- Libft/src/ft_putchar_fd.c
+++ Libft/src/ft_putchar_fd.c
@@ -6,14 +6,16 @@
 /*   By: hozhan <hozhan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 23:39:06 by hozhan            #+#    #+#             */
-/*   Updated: 2025/06/19 13:16:25 by hozhan           ###   ########.fr       */
+/*   Updated: 2025/06/19 13:25:13 by hozhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 /**
- * Outputs the character ’c’ to the specified file descriptor.
+ * @brief Outputs a single character to the specified file descriptor.
+ * @param c Character to be written
+ * @param fd File descriptor for the output operation
  */
 void	ft_putchar_fd(char c, int fd)
 {
