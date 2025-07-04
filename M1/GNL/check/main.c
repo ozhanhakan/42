8785cd83dc347222e63bfe27f5febd160f9fd956
hakan <hozhan42turkiye@gmail.com>
2025-07-04 13:24:29 +0300

    check main test

--- M1/GNL/check/main.c
+++ M1/GNL/check/main.c
@@ -3,24 +3,44 @@
 #include <stdio.h>          // printf fonksiyonu için (ekrana yazdırma)
 
 // Temel test main'i: test.txt dosyasını satır satır okur ve ekrana yazar.
+int test2(void);
+
 int main(void)
 {
     int fd = open("test.txt", O_RDONLY); // test.txt dosyasını okuma modunda aç
-    char *line; // Okunan satırı tutacak pointer
-    while ((line = get_next_line(fd))) // Satır satır oku
-    {
-        printf("%s", line); // Satırı ekrana yazdır
-        free(line);         // Belleği serbest bırak
+    char *line=NULL; // Okunan satırı tutacak pointer
+     line = get_next_line(fd);
+    while (!line) // Satır satır oku
+    {   
+    
+            printf("%s", line); // Satırı ekrana yazdır
+            free(line);         // Belleği serbest bırak
+         
+       line = get_next_line(fd);
     }
+    //printf("\nmain fd: %d\n", fd); // Dosya tanımlayıcısını ekrana yazdır
     close(fd); // Dosyayı kapat
+    
+    //int fd2 = open("test.txt", O_RDONLY); // test.txt dosyasını okuma modunda aç
+    //printf("\nmain fd: %d\n", fd2); // Dosya tanımlayıcısını ekrana yazdır
+    //close(fd2); // Dosyayı kapat
+    
+
+    test2();
+    //close(fd); // Dosyayı kapat
+//    printf("\nmain fd: after close %d\n", fd); // Dosya tanımlayıcısını ekrana yazdır
+    //close(fd2); // Dosyayı kapat
+    // close(fd3); // Dosyayı kapat
+    
     return 0; // Programı başarılı şekilde sonlandır
-    main2();
 }
-int main2(void)
+int test2(void)
 {
     int fd = open("test1.txt", O_RDONLY);
     char *line;
     line = get_next_line(fd);
+    printf("\nmain2 fd: %d\n", fd); // Dosya tanımlayıcısını ekrana yazdır
+
     while (line)
     {
         printf("%s", line);
